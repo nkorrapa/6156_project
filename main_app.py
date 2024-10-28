ebc0c6aecddc9f17eaf38f0773fb6e8d39615d74
Neha Korrapati <neha.korrapati@gmail.com>
2024-10-28 19:27:47 -0400

    added streamlit file

---
+++ main_app.py
@@ -0,0 +1,16 @@
+# import stuff
+import streamlit as st
+import numpy as np
+import pandas as pd
+#######################################
+
+# input box
+
+#############################
+
+## All the Logic and stuff
+
+###########################
+
+# avg delay
+# other options
