e56f20af609d75420ecef6e53b8156deb8e9db8e
Neha Korrapati <neha.korrapati@gmail.com>
2024-12-01 15:13:58 -0500

    adding models to streamlit

--- main_app.py
+++ main_app.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import time
 import datetime
+from functions.knn import preprocess_data, train_models
+from functions.linreg import add_colon, linreg_preprocess_data, train_linreg
 #####################################
 @st.cache_data()
 def load_file(file):
@@ -19,6 +21,7 @@ def load_file(file):
 # load look up table
 # input box
 delay = "MODEL OUTPUT"
+delay_reason = "KNN model output"
 flight_schedule = load_file('data/flight_schedule.csv')
 airport_cities = load_file('data/airport_cities.csv')
 airport_list = airport_cities[["AIRPORT"]].sort_values("AIRPORT")
@@ -38,7 +41,7 @@ airline = st.selectbox("Airline", airline_list, index = None, placeholder= "Sele
 ## All the Logic and stuff
 
 ###########################
-
+st.write("Your flight is usually delayed by " + delay_reason + ".")
 st.write("Your flight is delayed " + delay + " minutes on average.")
 
 st.write("Other Flight Options:")
